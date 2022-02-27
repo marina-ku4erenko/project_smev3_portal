b92a689a956d04948f64fc26b6a74bb019bba246
Marina Kucherenko <marina.ku4erenko@yandex.ru>
2022-02-27 21:45:35 +0300

    added Owner

--- src/test/java/mari/ku/tests/TestBase.java
+++ src/test/java/mari/ku/tests/TestBase.java
@@ -2,24 +2,28 @@ package mari.ku.tests;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
 import io.qameta.allure.selenide.AllureSelenide;
+import mari.ku.config.CredentialsConfig;
 import mari.ku.helpers.Attach;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
-import org.openqa.selenium.UnexpectedAlertBehaviour;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.WebDriverRunner.setWebDriver;
+import static java.lang.String.format;
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+
+        CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
+        Configuration.remote = format("https://%s:%s@%s",
+                credentials.login(),
+                credentials.password(),
+                System.getProperty("remoteUrl", "selenoid.autotests.cloud/wd/hub"));
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications");
@@ -31,15 +35,9 @@ public class TestBase {
         Configuration.browserCapabilities = capabilities;
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "91");
         Configuration.browserSize = "1280x1024";
-        //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
-
-        String remoteUrl = System.getProperty("remoteUrl");
-        String user = System.getProperty("user");
-        String password = System.getProperty("password");
-        Configuration.remote = "https://" + user + ":" + password + "@" + remoteUrl;
-
-
 
     }
 
